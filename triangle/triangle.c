f2e364d12d55e417d7929431bde3b412721afbe6
Mattéo <mcarton@student.s19.be>
2025-05-12 23:34:28 +0200

    Triangle : PASS

---
+++ triangle/triangle.c
@@ -0,0 +1,39 @@
+#include "triangle.h"
+
+static bool is_a_triangle(triangle_t sides)
+{
+    if (sides.a == 0 || sides.b == 0 || sides.c == 0)
+        return (false);
+    if (sides.a + sides.b >= sides.c &&
+        sides.b + sides.c >= sides.a &&
+        sides.a + sides.c >= sides.b)
+        return (true);
+    return (false);
+}
+
+bool is_equilateral(triangle_t sides)
+{
+    if (!is_a_triangle(sides))
+        return (false);
+    if (sides.a == sides.b && sides.b == sides.c)
+        return (true);
+    return (false);
+}
+
+bool is_isosceles(triangle_t sides)
+{
+    if (!is_a_triangle(sides))
+        return (false);
+    if (sides.a == sides.b || sides.b == sides.c || sides.a == sides.c)
+        return (true);
+    return (false);
+}
+
+bool is_scalene(triangle_t sides)
+{
+    if (!is_a_triangle(sides))
+        return (false);
+    if (sides.a != sides.b && sides.b != sides.c && sides.a != sides.c)
+        return (true);
+    return (false);
+}
